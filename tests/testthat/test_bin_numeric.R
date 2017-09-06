2c50bd4eb4c7e67ead8c5777d9758d0af1bdf7e0
louisfc <louis.vines@fundingcircle.com>
2017-09-06 10:12:48 +0100

    include bin_numeric for binning of numeric functions

---
+++ tests/testthat/test_bin_numeric.R
@@ -0,0 +1,114 @@
+binned_feature <- bin_numeric(german_credit_data, x = "credit_amount")
+
+test_that("bin_numeric creates an object of class binned_numeric", {
+  expect_is(binned_feature, "binned_numeric")
+  expect_is(binned_feature, "binned")
+})
+
+test_that("bin_numeric creates bins based on a supervised binning alg", {
+  expected_names <- c("feature", "feature_type", "cuts", "levels", "tree", "iv", "iv_table")
+
+  expect_equal(names(binned_feature), expected_names)
+  expect_equal_to_reference(binned_feature, "references/bin_numeric/supervised_binned.RDS")
+})
+
+test_that("bin_numeric can accept different names for y variable", {
+  german_data_different_y <- german_credit_data %>% rename(y = gb12)
+
+  binned_feature <- bin_numeric(german_data_different_y, x = "credit_amount", y = "y")
+
+  expect_equal_to_reference(binned_feature, "references/bin_numeric/supervised_binned_new_y.RDS")
+})
+
+test_that("if no significant splits are found, this is reported", {
+  binned_feature <- bin_numeric(german_credit_data, x = "existing_credits")
+
+  expect_equal(binned_feature, "No significant splits found")
+})
+
+test_that("create_numeric_supervised_bins can use a different y name", {
+  german_data_different_y <- german_credit_data %>% rename(y = gb12)
+
+  binned_feature <- create_numeric_supervised_bins(german_data_different_y, x = "credit_amount", y = "y")
+  expected_names_for_data_df <- c("group", "y")
+
+  expect_equal(names(binned_feature$data), expected_names_for_data_df)
+  expect_equal_to_reference(binned_feature, "references/numeric_supervised_binned_different_y.RDS")
+})
+
+test_that("tree_control can be used to pass parameters to ctree algorithm used for binning", {
+  binned_feature <- bin_numeric(german_credit_data, x = "credit_amount")
+
+  bin_with_small_tree <- create_numeric_supervised_bins(
+    german_credit_data,
+    x = "credit_amount",
+    tree_control = ctree_control(maxdepth = 1)
+  )
+
+  expect_gt(length(binned_feature$tree), length(bin_with_small_tree$tree))
+})
+
+test_that("if a bins argument is provided, bin_numeric creates specified number of bins
+           of roughly the same frequency", {
+  #example 1
+  n_bins <- 5
+
+  binned_feature <- bin_numeric(german_credit_data, x = "credit_amount", bins = n_bins)
+  binned_iv_table <- binned_feature$iv_table
+  bins_are_all_roughly_equal_check <- all(
+    round(binned_iv_table$freq / 1000, 2) == ((sum(binned_iv_table$freq) / n_bins) / 1000)
+  )
+
+  expect_equal(length(binned_feature$levels), n_bins)
+  expect_true(bins_are_all_roughly_equal_check)
+
+  #example 2
+  n_bins <- 10
+
+  binned_feature <- bin_numeric(german_credit_data, x = "credit_amount", bins = n_bins)
+
+  expect_equal(length(binned_feature$levels), n_bins)
+})
+
+test_that("if there are less unique values than bins requested, create_numeric_frequency_bins
+           uses the unique values as cutpoints", {
+  #the feature being binned takes four unique values
+  binned_feature <- bin_numeric(german_credit_data, x = "installment_rate_income", bins = 5)
+  binned_groups <- binned_feature$iv_table$group
+  expected_groups <- c("(,1]", "(1,2]", "(2,3]", "(3,]") %>% {factor(., levels = .)}
+
+  expect_equal(binned_groups, expected_groups)
+})
+
+test_that("if bins is not an integer >= 2 an error is raised", {
+  error_regex <- "must be integer"
+  expect_error(bin_numeric(feature_to_bin, bins=1), regexp=error_regex)
+  expect_error(bin_numeric(feature_to_bin, bins=-1), regexp=error_regex)
+  expect_error(bin_numeric(feature_to_bin, bins=3.2), regexp=error_regex)
+})
+
+test_that("binned_numeric objects have a predict method which bins the relevant column
+           on a new dataframe", {
+  feature_to_bin <- "credit_amount"
+
+  binned_feature <- bin_numeric(german_credit_data, x = feature_to_bin)
+  data_with_binned_feature <- predict(binned_feature, german_credit_data)
+
+  expect_true(all(data_with_binned_feature[[feature_to_bin]] %in% binned_feature$levels))
+})
+
+test_that("binned_numeric objects has a plot method that creates a graph with 3 elements", {
+  binned_feature <- bin_numeric(german_credit_data, x = "credit_amount")
+  binned_feature_plot <- plot(binned_feature)
+
+  expect_equal(length(binned_feature_plot$layers), 3)
+  purrr::walk(binned_feature_plot$layers, ~ expect_is(.x, "ggproto"))
+})
+
+test_that("if the original data set is also passed to the plot method the plot has 4 elements", {
+  binned_feature <- bin_numeric(german_credit_data, x = "credit_amount")
+  binned_feature_plot <- plot(binned_feature, german_credit_data)
+
+  expect_equal(length(binned_feature_plot$layers), 4)
+  purrr::walk(binned_feature_plot$layers, ~ expect_is(.x, "ggproto"))
+})
