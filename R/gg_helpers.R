b38369121b72a3abf71d323654311b7f9efef6b6
louis-vines <louisgabrielvines@gmail.com>
2017-11-20 22:31:21 +0530

    include function to add value labels to barplots

--- R/gg_helpers.R
+++ R/gg_helpers.R
@@ -6,3 +6,36 @@ remove_x_axis <- function(){
         axis.title.x = element_blank(),
         axis.line.x = element_blank())
 }
+
+gg_value_labels <- function(dframe, input_var_name, format_fn=identity, ...){
+  input_var <- as.symbol(input_var_name)
+  input_values <- dframe[[input_var_name]]
+  value_range <- range(input_values) %>% {.[2] - .[1]}
+
+  outside_lim <- value_range / 8
+  value_nudge <- value_range / 30
+
+  label_df <- dframe %>%
+    mutate(neg_inside = (!!input_var) %>% replace(. > - outside_lim, NA)) %>%
+    mutate(neg_outside = (!!input_var) %>% replace(. <= - outside_lim | . >= 0, NA)) %>%
+    mutate(pos_outside = (!!input_var) %>% replace(. < 0 | . >= outside_lim, NA)) %>%
+    mutate(pos_inside = (!!input_var) %>% replace(.  < outside_lim, NA)) %>%
+    mutate(!!input_var_name := format_fn(!!input_var))
+
+  numeric_label <- function(col_name, nudge_y, ...){
+    args <- list(mapping = aes_string(y = col_name, label = input_var_name),
+                 data = label_df,
+                 nudge_y = nudge_y,
+                 na.rm = TRUE,
+                 ...)
+
+    do.call(geom_text, args)
+  }
+
+  list(
+    numeric_label("neg_inside", value_nudge, colour = "white", ...),
+    numeric_label("neg_outside", -value_nudge, ...),
+    numeric_label("pos_outside", value_nudge, ...),
+    numeric_label("pos_inside", -value_nudge, colour = "white", ...)
+  )
+}
--- tests/testthat/test_gg_helpers.R
+++ tests/testthat/test_gg_helpers.R
@@ -6,3 +6,20 @@ test_that("remove_x_axis is a function to remove x axis elements from a ggplot o
 
   expect_identical(remove_x_axis(), expected_object)
 })
+
+test_that("gg_value_labels can be added to ggplot objects without throwing
+           an exception", {
+  example_data <- data_frame(
+    x = (-5:5) / 10,
+    y =  x^3
+  )
+
+  example_plot <- ggplot(example_data, aes(x, y)) +
+    geom_bar(stat = "identity")
+
+  expect_error(example_plot + gg_value_labels(example_data, "y"),
+               regexp = NA)
+
+  expect_error(example_plot + gg_value_labels(example_data, "y", size = 1.5),
+               regexp = NA)
+})
