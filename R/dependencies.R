14707f27acde3ef7ba786fe130f929a13fb4effe
louis-vines <louisgabrielvines@gmail.com>
2017-11-25 13:39:06 +0530

    include bin_numeric for binning of numeric functions

---
+++ R/dependencies.R
@@ -0,0 +1,8 @@
+#' @import dplyr
+#' @import ggplot2
+dependencies <- function(){
+  # This is a placeholder function to take care of importing whole packages that
+  # are reused across the codebase
+
+  NULL
+}
---
+++ R/gg_helpers.R
@@ -0,0 +1,8 @@
+library(ggplot2)
+
+remove_x_axis <- function(){
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.line.x = element_blank())
+}
